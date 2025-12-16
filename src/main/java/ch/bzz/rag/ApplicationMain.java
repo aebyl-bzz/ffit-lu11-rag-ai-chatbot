d690b86e57b36b457d4c39ba62e2c84d70020c7a
lucius <aebyl@bzz.ch>
2025-12-16 15:01:40 +0100

    Ajax Page Collector

--- src/main/java/ch/bzz/rag/ApplicationMain.java
+++ src/main/java/ch/bzz/rag/ApplicationMain.java
@@ -1,12 +1,17 @@
 package ch.bzz.rag;
 
+import ch.bzz.rag.service.*;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Set;
 
 @Slf4j
-@SpringBootApplication
 public class ApplicationMain {
+
     public static void main(String[] args) {
-        log.info("Application has been started");
+        WikiPageCollectorService collector = new WikiPageCollectorService();
+        String namespace = "de:modul:ffit:3-jahr:java:learningunits:lu11:";
+        Set<String> pages = collector.collectPagesForNamespace("https://wiki.bzz.ch", namespace);
+        log.info(pages.toString());
     }
 }
